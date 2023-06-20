2c029181ba23bdf68a572ee373275061a8aee9cb
c0a22028 <c0a220288a@edu.teu.ac.jp>
2023-06-20 15:22:11 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,13 +3,29 @@ import pygame as pg
 import random
 
 
-WIDTH, HEIGHT = 1600, 900
-delta = {
+WIDTH, HEIGHT = 1000, 600
+#キーごとの移動量の辞書
+delta = {  
     pg.K_UP:[0, -5],
     pg.K_DOWN:[0, 5],
     pg.K_LEFT:[-5, 0],
     pg.K_RIGHT:[5, 0]
 }
+
+
+def check_bound(rect: pg.Rect):
+    """
+    こうかとんRect、爆弾Rectが画面外か画面内かを判定する関数
+    引数：こうかとんRect or 爆弾Rect
+    戻り値：横方向、縦方向の判定結果タプル(True：画面内／False：画面内)
+    """
+    yoko, tate = True, True
+    if rect.left < 0 or WIDTH < rect.right:  #横宝庫の判定
+        yoko = False
+    if rect.top < 0 or HEIGHT < rect.bottom:  #縦方向の判定
+        tate = False
+    return (yoko, tate)
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -44,15 +60,26 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+                kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         screen.blit(bd_img, bd_rct)
         bd_rct.move_ip(vx, vy)
-        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        #こうかとんを更新前の位置に戻す
+        if not check_bound(kk_rct)[0]:
+            kk_rct.move_ip(-sum_mv[0], 0)
+        if not check_bound(kk_rct)[1]:
+            kk_rct.move_ip(0, -sum_mv[1])
+        #爆弾が画面外の場合、速度の符号を反転させる
+        if not check_bound(bd_rct)[0]:
+            vx *= -1
+        if not check_bound(bd_rct)[1]:
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 
 if __name__ == "__main__":
