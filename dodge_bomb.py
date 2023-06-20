490e9c72865e44e95aade913851fa81f5899c326
c0a22028 <c0a220288a@edu.teu.ac.jp>
2023-06-20 16:17:28 +0900

    演習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,7 @@ import pygame as pg
 import random
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1000, 600
 #キーごとの移動量の辞書
 delta = {  
     pg.K_UP:[0, -5],
@@ -32,7 +32,15 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    #左向きのこうかとん
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img4 = pg.transform.flip(kk_img, 1, 0)  #右
+    kk_img1 = pg.transform.rotozoom(kk_img, 315, 1.0)  #左上
+    kk_img2 = pg.transform.rotozoom(kk_img4, 90, 1.0)  #上
+    kk_img3 = pg.transform.rotozoom(kk_img4, 45, 1.0)  #右上
+    kk_img5 = pg.transform.rotozoom(kk_img4, 315, 1.0)  #右下
+    kk_img6 = pg.transform.rotozoom(kk_img4, 270, 1.0)  #下
+    kk_img7 = pg.transform.rotozoom(kk_img, 45, 1.0)  #左下
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bd_img = pg.Surface((20, 20))  #練習
@@ -46,6 +54,10 @@ def main():
     bd_rct = bd_img.get_rect()
     #爆弾rect(bg_rct)の中心座標を乱数で指定する
     bd_rct.center = x, y
+
+    kk_zisyo = {(-5, 0): kk_img, (-5, -5): kk_img1, (0, -5): kk_img2,
+                (5, -5): kk_img3, (5, 0): kk_img4, (5, 5): kk_img5,
+                (0, 5): kk_img6, (-5, 5): kk_img7,} 
 
 
     clock = pg.time.Clock()
@@ -64,14 +76,17 @@ def main():
         kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
+        for tup in kk_zisyo.keys():
+            if list(tup) == sum_mv:
+                screen.blit(kk_zisyo[tup], kk_rct)
+            elif sum_mv == [0, 0]:
+                screen.blit(kk_img, kk_rct)
         #こうかとんを更新前の位置に戻す
-        if not check_bound(kk_rct)[0]:
-            kk_rct.move_ip(-sum_mv[0], 0)
-        if not check_bound(kk_rct)[1]:
-            kk_rct.move_ip(0, -sum_mv[1])
-        bd_rct.move_ip(vx, vy)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
         #爆弾が画面外の場合、速度の符号を反転させる
+        bd_rct.move_ip(0,0)
         if not check_bound(bd_rct)[0]:
             vx *= -1
         if not check_bound(bd_rct)[1]:
